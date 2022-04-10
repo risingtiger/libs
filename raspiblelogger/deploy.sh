4858792d8e859d3a569038b7cecde06954a2645f
Dave <accounts@risingtiger.com>
2022-04-09 18:41:37 -0600

    removed blelogger

--- raspiblelogger/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-#rsync -avz --exclude 'node_modules' -e ssh ./ pi@192.168.1.171:/home/pi/raspblelogger/
-rsync -avz --exclude 'node_modules' -e -l ssh ./ pi@proxy21.rt3.io:/home/pi/raspblelogger/ -p 36192
-
